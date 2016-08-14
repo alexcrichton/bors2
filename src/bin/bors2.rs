cb8ec85616c95569ad2cc16b36f2c13f29c35620
Alex Crichton <alex@alexcrichton.com>
2016-08-14 15:19:53 -0700

    Draw a port from $PORT

--- src/bin/bors2.rs
+++ src/bin/bors2.rs
@@ -15,9 +15,12 @@ use std::net::SocketAddr;
 use futures_minihttp::{Server, Request, Response};
 
 fn main() {
-    let addr = env::args().nth(1).unwrap_or("127.0.0.1:8080".to_string());
+    let port = env::var("PORT").unwrap_or("8080".to_string());
+    let default_addr = format!("127.0.0.1:{}", port);
+    let addr = env::args().nth(1).unwrap_or(default_addr);
     let addr = addr.parse::<SocketAddr>().unwrap();
 
+    println!("Listening on {}", addr);
     Server::new(&addr).workers(1).serve(|_: Request| {
         let mut resp = Response::new();
         resp.body("hello");
