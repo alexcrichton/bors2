e0f9494f45fb1b17f51f4498a13c658296b056ed
Alex Crichton <alex@alexcrichton.com>
2016-10-26 13:21:35 -0700

    nginx fronted

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
-web: target/release/diesel migration run && ./target/release/bors2 0.0.0.0:$PORT
+web: target/release/diesel migration run && bin/start-nginx ./target/release/bors2 127.0.0.1:8888
 worker: target/release/diesel migration run && ./target/release/worker
--- src/bin/bors2.rs
+++ src/bin/bors2.rs
@@ -6,11 +6,17 @@ extern crate log;
 
 use std::env;
 use std::net::SocketAddr;
+use std::fs::File;
 
 use hyper::server::{Server, Request, Response};
 
 fn main() {
     env_logger::init().unwrap();
+
+    let heroku = env::var("HEROKU").is_ok();
+    if heroku {
+        File::create("/tmp/app-initialized").unwrap();
+    }
 
     let addr = env::args().nth(1).unwrap_or("127.0.0.1:3000".to_string());
     let addr = addr.parse::<SocketAddr>().unwrap();
